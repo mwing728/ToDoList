1421b002fd557ca6469e12abe172c431ed878006
Matthew Wing <wingm1@montclair.edu>
2020-01-04 23:04:21 -0500

    Updates

---
+++ precache-manifest.2782c5b242adea5d7121702fa878382c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97407c2a58ae17fe6a294d42a19c7391",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "e112255279dfc9a1c358",
+    "url": "/ToDoList/static/css/main.76910529.chunk.css"
+  },
+  {
+    "revision": "b26528f04216bc6c4426",
+    "url": "/ToDoList/static/js/2.b007b052.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/ToDoList/static/js/2.b007b052.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e112255279dfc9a1c358",
+    "url": "/ToDoList/static/js/main.278a47aa.chunk.js"
+  },
+  {
+    "revision": "134fba54665649774d71",
+    "url": "/ToDoList/static/js/runtime-main.cdef9c39.js"
+  }
+]);
